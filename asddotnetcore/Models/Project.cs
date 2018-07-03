b0c70d972d2b0a1c63af77594918e9ddfff3d9f9
Tomi Mannila <tomi.mannila@gmail.com>
2018-07-03 17:48:13 +0300

    Gave up on many to many relationship for now...

--- asddotnetcore/Models/Project.cs
+++ asddotnetcore/Models/Project.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,11 +17,25 @@ namespace MyWebApi.Models
         public string ImgAlt { get; set; }
         [StringLength(256, ErrorMessage = "Description cannot be longer than 256 characters.")]
         public string Description { get; set; }
-        //public Tool[] tools { get; set; }
+        //[ForeignKey("ProjectID")]
+        //public List<Tool> Tools { get; set; }
         [StringLength(512, ErrorMessage = "Details cannot be longer than 512 characters.")]
         public string Details { get; set; }
         public string[] Extraimg { get; set; }
+        
+        public List<int> Tools { get; set; }
+        //public ICollection<ProjectToTool> ProjectToTools { get; } = new List<ProjectToTool>();
     }
+
+
+    //public class ProjectToTool
+    //{
+    //    public int ProjectId { get; set; }
+    //    public Project Project { get; set; }
+
+    //    public int ToolId { get; set; }
+    //    public Tool Tool { get; set; }
+    //}
 }
 
 /**

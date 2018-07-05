f31575511e2ecb4c27699d45e6fc2bf944c11762
Tomi Mannila <tomi.mannila@gmail.com>
2018-07-05 20:14:19 +0300

    added jwt to project + configured it

--- asddotnetcore/Startup.cs
+++ asddotnetcore/Startup.cs
@@ -17,23 +17,39 @@ using Microsoft.Extensions.Options;
 using MyIdentity.Models;
 using MyWebApi.Models;
 using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
 
 namespace asddotnetcore
 {
     public class Startup
     {
+        private string _secretKey;
+        private SymmetricSecurityKey _signinKey;
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
-            
+            Configuration = configuration;          
         }
-        
 
         public IConfiguration Configuration { get; }
+
+        
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Get SecretKey and generate new symmetric key
+            _secretKey = Configuration["SecretKey"];
+            _signinKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_secretKey));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // TODO DON'T ALLOW ALL SITES TO ACCESS
@@ -70,8 +86,45 @@ namespace asddotnetcore
             {
                 options.Issuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)];
                 options.Audience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)];
-                //options.SigningCredentials = new SigningCredentials(_signinKey, SecurityAlgorithms.HmacSha256);
+                options.SigningCredentials = new SigningCredentials(_signinKey, SecurityAlgorithms.HmacSha256);
             });
+
+            // Configure token validation parameters
+            // https://blogs.msdn.microsoft.com/webdev/2017/04/06/jwt-validation-and-authorization-in-asp-net-core/
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)],
+
+                ValidateAudience = true,
+                ValidAudience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)],
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = _signinKey,
+
+                RequireExpirationTime = false,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero, // remove delay of token when expire
+            };
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(configureOptions =>
+            {
+                configureOptions.ClaimsIssuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)];
+                configureOptions.TokenValidationParameters = tokenValidationParameters;
+                configureOptions.SaveToken = true;
+            });
+
+            //services.AddAuthorization(options =>
+            //{
+            //    // checks for the presence of "Id" in the identity aka. every identity has "Admin" -status
+            //    options.AddPolicy("Admin", policy => policy.RequireClaim("Id"));
+            //    JwtConstants.
+            //});
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -106,6 +159,7 @@ namespace asddotnetcore
             //    .AllowAnyOrigin()
             //    );
 
+            app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
